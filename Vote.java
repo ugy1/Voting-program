f98f7fe3d0794c5b657496d78de132e675f1891f
ugy1 <134857261+ugy1@users.noreply.github.com>
2024-06-14 23:07:34 -0400

    Create Vote.java

---
+++ Vote.java
@@ -0,0 +1,11 @@
+public class Vote {
+    private int candidateId;
+
+    public Vote(int candidateId) {
+        this.candidateId = candidateId;
+    }
+
+    public int getCandidateId() {
+        return candidateId;
+    }
+}
